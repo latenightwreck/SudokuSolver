ce7908dad4f0644a18e7f0dc45ca4e495d5b6b40
latenightwreck <morgan.b.armstrong@gmail.com>
2018-08-11 16:45:13 -0400

    Switch value to use constant

--- src/FileToArrayConverter.java
+++ src/FileToArrayConverter.java
@@ -3,6 +3,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+import static constants.Constants.EMPTY;
 import static constants.Constants.SIZE;
 
 public class FileToArrayConverter {
@@ -25,7 +26,7 @@ public class FileToArrayConverter {
                 //if the value in the location is not a number (in the instance of this test, it can be an 'X')
                 //replace with zero (0), as this can not be on the board when finished, and still holds an int value
                 if (!StringUtils.isNumeric(tokens[i])) {
-                    array[row][i] = 0;
+                    array[row][i] = EMPTY;
                 } else {
                     array[row][i] = Integer.parseInt(tokens[i]);
                 }
