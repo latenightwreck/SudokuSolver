49f5b57977bd5451ea6a9a13a5a3ac9a21adfea1
latenightwreck <morgan.b.armstrong@gmail.com>
2018-08-12 18:27:33 -0400

    Added SUBSECTION_SIZE and SOLUTION_FILE variables

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -2,5 +2,7 @@ package constants;
 
 public final class Constants {
     public static int SIZE = 9;
+    public static int SUBSECTION_SIZE = 3;
     public static int EMPTY = 0;
+    public static String SOLUTION_FILE=".sln.txt";
 }
