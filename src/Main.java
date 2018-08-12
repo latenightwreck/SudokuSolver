9dd012af05e5e5d021091e44d9ef0736687f5518
latenightwreck <morgan.b.armstrong@gmail.com>
2018-08-12 18:48:14 -0400

    If solved, print board to a txt file in sresources/solutions

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,33 @@
+import constants.Constants;
 import org.apache.commons.io.FilenameUtils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
-        File dir = new File("resources");
+        File dir = new File("resources/puzzles");
         for (File file : dir.listFiles()) {
+            //try-with-resources will auto close the file, even on Exception
             try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                //Get filepath, as it is used in multiple locations
                 String selectedFilePath = file.getAbsolutePath();
+                //Make sure the input file is a txt file, else, skip it
                 if (!FilenameUtils.getExtension(selectedFilePath).equals("txt")) {
-                    System.out.println("Sorry, please select a txt file!");
+                    System.out.println("Sorry, please use a txt file!");
                     return;
                 } else {
                     FileToArrayConverter converter = new FileToArrayConverter();
                     Sudoku sudoku = new Sudoku(converter.convertFile(bufferedReader));
                     if (sudoku.solve()) {
-                        System.out.println(sudoku.toString());
+                        //The file for the solution is created in resources/solutions
+                        //with the original fileName+.sln.txt
+                        String solutionFilePath = "resources/solutions/"+FilenameUtils.getBaseName(selectedFilePath)+ Constants.SOLUTION_FILE;
+                        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(solutionFilePath))) {
+                            bufferedWriter.write(sudoku.printBoard());
+                        }
                     } else {
                         System.out.println("This puzzle is unsolvable! The file name is: "+file.getName());
                     }
@@ -31,8 +37,7 @@ public class Main {
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-
         }
-
+        System.out.println("All files have been run! Please check resources/solutions for the answers!");
     }
 }
